0ffe858a5cd357247d908844d18da9573b42b7c3
AidarSource <34319725+AidarSource@users.noreply.github.com>
2021-08-22 02:27:33 +0600

    Upload final build

---
+++ dnn.py
@@ -0,0 +1,175 @@
+import torch
+import torch.nn as nn
+import pandas as pd
+import numpy as np
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from sklearn.model_selection import train_test_split
+import torch.nn.functional as F
+
+
+torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Prepare Dataset
+# load data
+train = pd.read_csv(r"train.csv", dtype=np.float32)
+
+# split data into features(pixels) and labels(numbers from 0 to 9)
+targets_numpy = train.label.values
+features_numpy = train.loc[:, train.columns != "label"].values / 255  # normalization
+
+t_size = 0.05
+# train test split. Size of train data is 80% and size of test data is 20%.
+features_train, features_test, targets_train, targets_test = train_test_split(features_numpy,
+                                                                              targets_numpy,
+                                                                              test_size=t_size,
+                                                                              random_state=42)
+print("Test size: " + str(t_size))
+
+# create feature and targets tensor for train set. As you remember we need variable to accumulate gradients.
+# Therefore first we create tensor, then we will create variable
+featuresTrain = torch.from_numpy(features_train)
+targetsTrain = torch.from_numpy(targets_train).type(torch.LongTensor)  # data type is long
+
+# create feature and targets tensor for test set.
+featuresTest = torch.from_numpy(features_test)
+targetsTest = torch.from_numpy(targets_test).type(torch.LongTensor)  # data type is long
+
+
+# Create CNN Model
+class CNNModel(nn.Module):
+    def __init__(self):
+        super(CNNModel, self).__init__()
+
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 4 * 4, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))  # -> n, 6, 14, 14
+        x = self.pool(F.relu(self.conv2(x)))  # -> n, 16, 5, 5
+        x = x.view(x.size(0), 16 * 4 * 4)  # -> n, 400
+        x = F.relu(self.fc1(x))  # -> n, 120
+        x = F.relu(self.fc2(x))  # -> n, 84
+        x = self.fc3(x)  # -> n, 10
+        return x
+
+
+# batch_size, epoch and iteration
+batch_size = 30 # 100
+print("Batch size: " + str(batch_size))
+n_iters = 50000
+num_epochs = n_iters / (len(features_train) / batch_size)
+num_epochs = int(num_epochs)
+
+# Pytorch train and test sets
+train = torch.utils.data.TensorDataset(featuresTrain, targetsTrain)
+test = torch.utils.data.TensorDataset(featuresTest, targetsTest)
+
+
+# data loader
+train_loader = torch.utils.data.DataLoader(train, batch_size=batch_size, shuffle=False)
+test_loader = torch.utils.data.DataLoader(test, batch_size=batch_size, shuffle=False)
+
+
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+    print('cuda activated!')
+else:
+    device = torch.device('cpu')
+    print('cpu activated!')
+
+# Create CNN
+model = CNNModel().to(device)
+
+
+
+# Cross Entropy Loss
+error = nn.CrossEntropyLoss()
+
+# SGD Optimizer
+learning_rate = 0.1
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+# CNN model training
+count = 0
+loss_list = []
+iteration_list = []
+accuracy_list = []
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+
+        images = images.to(device)
+        labels = labels.to(device)
+
+
+        train = Variable(images.view(30, 1, 28, 28)) # 100
+        labels = Variable(labels)
+
+        # Clear gradients
+        optimizer.zero_grad()
+
+        # Forward propagation
+        outputs = model(train)
+
+        # Calculate softmax and ross entropy loss
+        loss = error(outputs, labels)
+
+        # Calculating gradients
+        loss.backward()
+
+        # Update parameters
+        optimizer.step()
+
+        count += 1
+
+        if count % 50 == 0:
+            # Calculate Accuracy
+            correct = 0
+            total = 0
+            # Iterate through test dataset
+            for images, labels in test_loader:
+                images = images.to(device)
+                labels = labels.to(device)
+
+                test = Variable(images.view(30, 1, 28, 28))
+
+                # Forward propagation
+                outputs = model(test)
+
+                # Get predictions from the maximum value
+                predicted = torch.max(outputs.data, 1)[1]
+
+                # Total number of labels
+                total += len(labels)
+
+                correct += (predicted == labels).sum()
+
+            accuracy = 100 * correct / float(total)
+
+            # store loss and iteration
+            loss_list.append(loss.data)
+            iteration_list.append(count)
+            accuracy_list.append(accuracy)
+        if count % 500 == 0:
+            # Print Loss
+            print('Iteration: {}  Loss: {}  Accuracy: {} %'.format(count, loss.data, accuracy))
+
+
+
+test_df = pd.read_csv("test.csv", dtype=np.float32)
+
+Xt_1 = (test_df.values/255).reshape((-1, 1, 28, 28))
+sample_sub = pd.read_csv("sample_submission.csv")
+
+with torch.no_grad():
+    model.eval()
+    sample_sub['Label'] = model(torch.from_numpy(Xt_1).to(device)).cpu().argmax(dim=1)
+
+
+
+sample_sub.to_csv("submission.csv", index=False)
+
